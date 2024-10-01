899d3ee0a68bc223b3bae420c3ba9a5c01e27d3c
AJ Ashik <uniquecollection163@gmail.com>
2024-10-01 11:05:57 +0600

    Delete streamhub/config.js

--- streamhub/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBwBO3nxN--TxB8JSuw7EWvnk6ckhjrSiU",
-    authDomain: "streamhub-live-tv-2.firebaseapp.com",
-    projectId: "streamhub-live-tv-2",
-    storageBucket: "streamhub-live-tv-2.appspot.com",
-    messagingSenderId: "565778821210",
-    appId: "1:565778821210:web:99c48d670f5eb94341a863"
-};
