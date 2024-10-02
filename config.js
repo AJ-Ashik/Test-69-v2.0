520717992e5e4336ddb8349824f2c7faace1a208
AJ Ashik <uniquecollection163@gmail.com>
2024-10-03 00:01:46 +0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "AUTH_DOMAIN",
-    projectId: "PROJECT_ID",
-    storageBucket: "STORAGE_BUCKET",
-    messagingSenderId: "MESSA_ID",
-    appId: "APP_ID"
-};
