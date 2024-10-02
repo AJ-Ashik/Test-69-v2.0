c6fb2d78785a536cbdcb360c28baa98f524dde76
AJ Ashik <uniquecollection163@gmail.com>
2024-10-02 18:45:17 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBwBO3nxN--TxB8JSuw7EWvnk6ckhjrSiU",
-    authDomain: "streamhub-live-tv-2.firebaseapp.com",
-    projectId: "streamhub-live-tv-2",
-    storageBucket: "streamhub-live-tv-2.appspot.com",
-    messagingSenderId: "565778821210",
-    appId: "1:565778821210:web:99c48d670f5eb94341a863"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSA_ID",
+    appId: "APP_ID"
 };
